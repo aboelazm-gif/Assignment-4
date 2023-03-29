de7ca38f8ebba0b61bf6594e3d79746a19d03d81
Kareem Abo Elazm <3aseerelsabbar@gmail.com>
2023-03-29 05:36:48 +0200

    s4q2-q3

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package Q2;
-
 import java.util.ArrayList;
 
 public class Main {
@@ -13,6 +11,7 @@ public class Main {
         list.add(10);
         list.add(30);
         System.out.println(max(list));
+        sort(list);
     }
 
     public static Integer max(ArrayList<Integer> list)
@@ -28,6 +27,26 @@ public class Main {
                     max= list.get(i);
             }
             return max;
+        }
+    }
+    public static void sort(ArrayList<Integer>list)
+    {
+        int tmp = 0;
+        for(int i=0;i<list.size();i++)
+        {
+            for(int j=i+1;j<list.size();j++)
+            {
+                if(list.get(i)>list.get(j))
+                {
+                    tmp=list.get(i);
+                    list.set(i,list.get(j));
+                    list.set(j,tmp);
+                }
+            }
+        }
+        for(int i=0;i<list.size();i++)
+        {
+            System.out.println(list.get(i)+"\n");
         }
     }
 }
